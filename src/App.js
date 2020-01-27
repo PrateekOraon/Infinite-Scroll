8642c89f8db2af403d345dc11f33cfecb31abdc2
Prateek <oraonprateek251@gmail.com>
2020-01-28 01:41:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+let page_number;
+let total;
 
 function App() {
+  const [data, updateData] = useState([]);
+  const [isLoading, updateIsLoading] = useState(true);
+  const [errorLoading, updateErrorLoading] = useState(false);
+  const [hasEnded, updateEnded] = useState(false);
+
+  useEffect(() => {
+    page_number = 1;
+    setTimeout(() => {
+      fetch(`https://reqres.in/api/unknown?per_page=2`)
+        .then(resp => resp.json())
+        .then(resp => {
+          total = resp.total_pages;
+          updateData([...data, ...resp.data]);
+          updateIsLoading(false);
+        })
+        .catch(err => updateErrorLoading(true));
+    }, 1000);
+  }, []);
+
+  const loadItems = page => {
+    setTimeout(() => {
+      fetch(`https://reqres.in/api/unknown?per_page=2&page=${page}`)
+        .then(resp => resp.json())
+        .then(resp => {
+          updateData([...data, ...resp.data]);
+          updateIsLoading(false);
+        })
+        .catch(err => updateErrorLoading(true));
+    }, 500);
+  };
+
+  window.onscroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      page_number = page_number + 1;
+      if (page_number > total) {
+        updateEnded(true);
+        return;
+      }
+      updateIsLoading(true);
+      loadItems(page_number);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Heading">INFINITE SCROLL</div>
+        <>
+          {data.map(item => {
+            return (
+              <div
+                key={item.id}
+                className="item"
+                style={{ background: item.color }}
+              >
+                {item.name}
+              </div>
+            );
+          })}
+          {errorLoading ? (
+            <div>Can't fetch data</div>
+          ) : isLoading ? (
+            <div>loading...</div>
+          ) : null}
+          {hasEnded ? <div>No more items!</div> : null}
+        </>
       </header>
     </div>
   );
